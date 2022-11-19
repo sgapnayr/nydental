fd10d17e72b1dcc3cf01330c9b504cd434e90ae7
Ryan Paglione <ryanpaglionework@gmail.com>
2022-11-19 17:27:20 -0600

    Hover Bug

--- Components/Home/Info2/Info2.tsx
+++ Components/Home/Info2/Info2.tsx
@@ -30,23 +30,40 @@ function Info2() {
                         <div className={styles.TextDesc}>
                             While our all-inclusive dental services are great, what makes us stand out is our imagination of the dentistry experience. From our advanced technology to our high-end interior and finishes, we have designed every detail to exceed your expectations of a clinical appointment. Schedule your consultation today for luxe dental care that will change the way you view dentistry.
                         </div>
-                        <Link href={'/About'} className={styles.Button}>Learn more</Link>
+                        <Link href={'/Location'} className={styles.LearnButton}>Learn more</Link>
                     </div>
                 </div>
             </div>
+
             <div className={styles.Info2}>
                 <div className={styles.RightSide}></div>
                 <div className={styles.LeftSide}>
                     <Image className={styles.Logo} src={Logo} alt={''} />
                 </div>
             </div>
+
             <div className={styles.Info2}>
+                <Image className={styles.Image1} src={Image1} alt={''} />
+                <Image className={styles.Image2} src={Image2} alt={''} />
+                <Image className={styles.Image3} src={Image3} alt={''} />
                 <div className={styles.LeftSide}>
                     <Image className={styles.Logo} src={Logo} alt={''} />
                 </div>
                 <div className={styles.RightSide}>
+                    <div className={styles.TextBox}>
+                        <div className={styles.Header}>
+                            <strong>
+                                Dentistry Elevated
+                            </strong>
+                        </div>
+                        <div className={styles.TextDesc}>
+                            While our all-inclusive dental services are great, what makes us stand out is our imagination of the dentistry experience. From our advanced technology to our high-end interior and finishes, we have designed every detail to exceed your expectations of a clinical appointment. Schedule your consultation today for luxe dental care that will change the way you view dentistry.
+                        </div>
+                        <Link href={'/About'} className={styles.LearnButton}>Learn more</Link>
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
